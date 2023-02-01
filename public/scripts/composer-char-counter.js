b4d1602d23f00424455b081574140202014effee
Sam <sammynarduzzi@gmail.com>
2023-01-31 20:32:26 -0800

    add char count function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  console.log("DOM is ready");
+});
+
+$(document).ready(function() {
+  $("textarea").keyup(function() {
+
+    let charCount = $(this).val().length;
+    let tweet = $(this).parents(".new-tweet").first();
+    let counter = tweet.children(".counter");
+
+    if (charCount <= 140) {
+      counter.removeClass("neg-counter").html(140 - charCount);
+    } else {
+      counter.addClass("neg-counter").html(140 - charCount);
+    }
+  });
 });
