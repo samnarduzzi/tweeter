1fad83d64ee0c8e21ab942b674015be0e97adaf3
Sam <sammynarduzzi@gmail.com>
2023-02-02 15:04:53 -0800

    newest tweets now appear at the top, take out commented stuff that is not needed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,4 @@
-// const { create } = require("domain");
-
 $(document).ready(function() {
-
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png",
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1675119835294
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd"
-  //     },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1675206235294
-  //   }
-  // ];
-
 
   const createTweetElement = function(tweet) {
     const $tweet = $(`
@@ -59,21 +31,21 @@ $(document).ready(function() {
       </footer>
     </article>
   `);
-    /* Your code for creating the tweet element */
 
     return $tweet;
   };
 
 
   const renderTweets = function(tweets) {
+    $(".tweet-container").empty();
+
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
-
+    
     for (let tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      // console.log($tweet);
-      $(".tweet-container").append($tweet);
+      $(".tweet-container").prepend($tweet);
     }
 
   };
